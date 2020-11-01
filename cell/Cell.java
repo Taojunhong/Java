5ac6eeb807d48051c74d7084c4b520275f276cf1
陶军红 <2861064505@qq.com>
2020-11-02 01:15:55 +0800

    细胞自动机

---
+++ cell/Cell.java
@@ -0,0 +1,20 @@
+package cell;
+
+//Ï¸°ûÉú³ÉÆ÷
+
+import java.awt.Graphics;
+ 
+public class Cell {
+	private boolean alive = false;
+	
+	public void die() { alive = false; }
+	public void reborn() { alive = true; }
+	public boolean isAlive() { return alive; }
+	
+	public void draw(Graphics g, int x, int y, int size) {
+		g.drawRect(x, y, size, size);
+		if ( alive ) {
+			g.fillRect(x, y, size, size);
+		}
+	}
+}
